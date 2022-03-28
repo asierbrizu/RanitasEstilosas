fffe7aaffc96bd39f26ef0cd3a654cbb293e6254
Asier Brizuela <asierbrizu@opendeusto.es>
2022-03-28 13:48:22 +0200

    Arreglos varios

--- Proyecto1/problem/maze.R
+++ Proyecto1/problem/maze.R
@@ -1,31 +1,35 @@
 # This function must return a list with the information needed to solve the problem.
 initialize.problem <- function(file) {
-  print(file)
+  file2="feet-maze-1a.txt"
+  print(file2)
   problem <- list()
   # Compulsory attributes
   problem$name              <- "Laberinto"
-  problem$maze              <- read.delim(file.choose())
-  print(problem$maze)
-  filas=strsplit(problem$maze[1],split=";")[1]
-  columnas=strsplit(maze[1],split=";")[2]
-  filaInicio=strsplit(maze[filas+2],split=";")[1]
-  colInicio=strsplit(maze[filas+2],split=";")[2]
-  filaFinstrsplit(maze[filas+3],split=";")[1]
-  colFinstrsplit(maze[filas+3],split=";")[2]
+  problem$maze              <- read.table(file2,header=FALSE,sep="\n")
+#  problem$maze              <- read.csv(file2,header=FALSE)
+  print(problem$maze[1,])
+  filas=strtoi(strsplit(problem$maze[1,],split=";")[1])
+  print(problem$maze[1,])
+  filas=strtoi(unlist(strsplit(problem$maze[1,],split=";"))[1])
+  columnas=strtoi(unlist(strsplit(problem$maze[1,],split=";"))[2])
+  filaInicio=strtoi(unlist(strsplit(problem$maze[filas+2,],split=";"))[1])
+  colInicio=strtoi(unlist(strsplit(problem$maze[filas+2,],split=";"))[2])
+  filaFin=strtoi(unlist(strsplit(problem$maze[filas+3,],split=";"))[1])
+  colFin=strtoi(unlist(strsplit(problem$maze[filas+3,],split=";"))[2])
   laberinto=rep(NA,filas)
   for(i in filas){
-    laberinto[i]<-strsplit(maze[i+1],split=";")
+    laberinto[i]<-strsplit(problem$maze[i+1],split=";")
     print(laberinto[i])
   }
-  #barIzda=maze[filas+4]
-  #barDrcha
-  #barArr
-  #barAbj
+  barIzda=strsplit(problem$maze[filas+4],split=";")
+  barDrcha=strsplit(problem$maze[filas+5],split=";")
+  barAbj=strsplit(problem$maze[filas+6],split=";")
+  barArr=strsplit(problem$maze[filas+7],split=";")
   
   
 
-    problem$state_initial     <- problem$maze[mazeOk[1] + 2]
-  problem$state_final       <- problem$maze[mazeOk[1] + 3]
+  problem$state_initial     <- problem$maze[filas+2]
+  problem$state_final       <- problem$maze[filas+3]
 
   
   # There are 4 actions that move: UP, DOWN, LEFT, RIGHT
@@ -39,10 +43,34 @@ initialize.problem <- function(file) {
 # There is an IF for each action.
 is.applicable <- function (state, action, problem) {
   print(problem$name)
-  
+  filaActual=strsplit(state,split=",")[1]
+  columnaActual=strsplit(state,split=",")[2]
   if (action == "Up") {
-    return(row != 1)
-  }
+    #Comprobar que no hay vacio
+    if(filaActual==0){
+      #PROBLEMA
+    }else{
+      #Comprobar que sean pies distintos
+      if(laberinto[filaActual,columnaActual]==laberinto[filaActual-1,columnaActual]){
+        #PROBLEMA
+      }else{
+        #Comprobar barreras
+        barrerasIzda=strsplit(maze[filas+4],split=";")
+        for(barrera in barrerasIzda){
+          if(state==barrera){
+            #PROBLEMON
+          }
+          split(maze[fila[2]+5],barrerasDrcha,drop=FALSE,sep=";",lex.order=FALSE)
+          barrerasDrcha=strsplit(maze[filas+5],split=";")
+          
+            for(barrera in barrerasDrcha){
+            
+            if(paste(filaActual,columnaActual-1,sep=",")==barrera){####HAY QUE COMPROBARLO
+              #PROBLEMON
+            }
+          }
+        }
+      }}}
   
   if (action == "Down") {
     return(row != problem$rows)
