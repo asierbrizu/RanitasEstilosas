639fc8208064910920f3ea937229c0f7e8649cd9
Iker Morales <ikermorales@opendeusto.es>
2022-04-24 12:45:42 +0200

    Todo hecho
    Añadido todo

--- Proyecto2/algorithms/informed/random-restart-hill-climbing.R
+++ Proyecto2/algorithms/informed/random-restart-hill-climbing.R
@@ -1,43 +1,34 @@
-RRHC = function(problem, iterations, repetitions){
-  result = list(method = "RRHC", final.state = c(),status = "",
-                max.frontier = 0, max.depth = 0, iterations = 0)
- n = floor(runif(1, min=0, max=40));
-  node = list(parent=c(), state=n, actions=c(), depth=0, cost=0,
-              evaluation = get.evaluation(n, problem))
-  frontier = list(node)
- 
-  count = 1
-  count.limit = iterations
-
-  while (TRUE){
-    if (length(frontier)==0){result$status = "No nodes in the frontier. No solution possible.";break}
-    if (count==count.limit){result$status = "Maximum number of iterations reached";break}
-    
-   
-    firstnode = frontier[[1]]; 
-    frontier[[1]] = NULL  
-
-    if (is.final.state(firstnode$state,problem)){result$status="Solution Found";break}
-
-    newnodes = expand.node(firstnode, problem) 
+random.restart.hill.climbing = function(file, p, iterations) {
   
-    newnodes = newnodes[order(sapply(newnodes,function (x) x$evaluation))]
-    newnode = newnodes[[1]]
-    
-    print(glue("The current node is {firstnode$evaluation}, the best successor is {newnode$evaluation}"))
-    if (firstnode$evaluation > newnode$evaluation){
-      frontier = list(newnode)
-    } else{
-      result$status="Local Optimum Found";break
-    }
-
-    result$max.frontier = max(result$max.frontier,length(frontier))
-    result$max.depth = max(result$max.depth,firstnode$depth)
-    result$iterations = count
-
-    count = count+1
+  # Get Start time
+  start_time       <- Sys.time()
+  
+  #Initialization of information for further analysis
+  report <- data.frame(iteration = numeric())
+  
+  results <- vector(mode = "list", length = iterations)
+  
+  problem <- initialize.problem(p = p, filename = file)
+  
+  count <- 1
+  while (count <= iterations) {
+    results[[count]] <- hill.climbing.search(problem = problem)
+    report <- rbind(report, data.frame(iteration = count))
+    count <- count + 1
   }
-
-  result$final.state = firstnode
+  
+  # Analyze results
+  results_df        <- local.analyze.results(results, problem)
+  best_result_index <- match(min(results_df$Evaluation), results_df$Evaluation)
+  
+  # Get runtime
+  end_time <- Sys.time()
+  
+  result             <- list()
+  result$name        <- paste0("Random Restart Hill Climbing")
+  result$runtime     <- end_time - start_time
+  result$state_final <- results[[best_result_index]]$state_final
+  result$report      <- report
+  
   return(result)
 }
--- Proyecto2/main/main-p-hub.R
+++ Proyecto2/main/main-p-hub.R
@@ -13,6 +13,7 @@ library(magrittr)
 source("../algorithms/blind/expand-node.R")
 source("../algorithms/informed/hill-climbing-search.R")
 source("../algorithms/informed/random-restart-hill-climbing.R")
+source("../algorithms/informed/local-beam-search.R")
 
 # Include functions for data analysis and result plot
 source("../algorithms/results-analysis/analyze-results.R")
@@ -20,88 +21,60 @@ source("../algorithms/results-analysis/analyze-results.R")
 # Include the problem
 source("../problem/p-hub-problem.R")
 
-# Executes hill climbing search and return the results
-execute.hill.climbing <- function(filename, p) {
-  # Initialize problem
-  problem <- initialize.problem(p = p, filename = filename)
-  # Execute hill climbing
-  return(hill.climbing.search(problem = problem))
-}
-
-# Ejecuta el random hill climbing
-random.restart.hill.climbing <- function(file, p, times) {
-  # Execute hill climbing 'n' times
-  results <- vector(mode = "list", length = times)
-  
-  for (i in 1:times) {
-    results[[i]] <- execute.hill.climbing(filename = file, p = p)
-  }
-  
-  # Initialize a problem instance for the analysis
-  problem <- initialize.problem(filename = file, p = p)
-  
-  # Analyze results
-  results_df <- local.analyze.results(results, problem)
-  
-  print(paste0("Best evaluation: ", round(min(results_df$Evaluation), 2), 
-               " - Mean: ", round(mean(results_df$Evaluation), 2), 
-               " - SD: ", round(sd(results_df$Evaluation), 2)), quote = FALSE)
-  print(paste0("Best runtime: ", round(min(results_df$Runtime), 2), 
-               " - Mean: ", round(mean(results_df$Runtime), 2), 
-               " - SD: ", round(sd(results_df$Runtime), 2)), quote = FALSE)
-  
-  return(results_df)
-}
-
-# Execute Hill Climbing several times and analyze results
-test.hill.climbing <- function(file, p, times) {
-  # Execute hill climbing 'n' times
-  results <- vector(mode = "list", length = times)
-  
-  for (i in 1:times) {
-    results[[i]] <- execute.hill.climbing(filename = file, p = p)
-  }
-  
-  # Initialize a problem instance for the analysis
-  problem <- initialize.problem(filename = file, p = p)
-  
-  # Analyze results
-  results_df <- local.analyze.results(results, problem)
-  
-  print(paste0("Best evaluation: ", round(min(results_df$Evaluation), 2), 
-               " - Mean: ", round(mean(results_df$Evaluation), 2), 
-               " - SD: ", round(sd(results_df$Evaluation), 2)), quote = FALSE)
-  print(paste0("Best runtime: ", round(min(results_df$Runtime), 2), 
-               " - Mean: ", round(mean(results_df$Runtime), 2), 
-               " - SD: ", round(sd(results_df$Runtime), 2)), quote = FALSE)
-  
-  return(results_df)
-}
-
 # Clear console
 cat("\014")
 graphics.off()
 
-file        <- "../data/p-hub/AP40.txt"
-p           <- 4
-times       <- 10
-results_df  <- test.hill.climbing(file, p, times)
+test <- 7
+
+all_results  <- matrix(list(), nrow=test, ncol=10)
+best_results <- vector(mode = "list", length = test)
+
+file <- "../data/p-hub/AP40.txt"
+p    <- 4
+
+problem <- initialize.problem(p = p, filename = file)
+
+for(i in 1:10){
+  
+  #Hill Climbing Search
+  all_results[[1,i]] <- hill.climbing.search(problem = problem)
+  
+  #Random Restart Hill Climbing
+  times              <- 1
+  all_results[[2,i]] <- random.restart.hill.climbing(file, p, times)
+  times              <- 2
+  all_results[[3,i]] <- random.restart.hill.climbing(file, p, times)
+  times              <- 3
+  all_results[[4,i]] <- random.restart.hill.climbing(file, p, times)
+  
+  #Local Beam Search
+  beams              <- 1
+  all_results[[5,i]] <- local.beam.search(problem, beams)
+  beams              <- 2
+  all_results[[6,i]] <- local.beam.search(problem, beams)
+  beams              <- 3
+  all_results[[7,i]] <- local.beam.search(problem, beams)
+  
+  print(paste0(i, " loop"))
+}
+
+for (i in 1:test) {
+  results <- local.analyze.results(all_results[i,], problem)
+  
+  print(all_results[[i,1]]$name)
+  print(paste0("Best evaluation: ", round(min(results$Evaluation), 2), 
+               " - Mean: ", round(mean(results$Evaluation), 2), 
+               " - SD: ", round(sd(results$Evaluation), 2)), quote = FALSE)
+  print(paste0("Best runtime: ", round(min(results$Runtime), 2), 
+               " - Mean: ", round(mean(results$Runtime), 2), 
+               " - SD: ", round(sd(results$Runtime), 2)), quote = FALSE)
+  print("---------------------------------------------------------------------")
+  
+  best_result_index <- match(min(results$Evaluation), results$Evaluation)
+  best_results[[i]]   <- all_results[[i,best_result_index]]
+}
+
 # Print results in an HTML Table
+results_df  <- local.analyze.results(best_results, problem)
 kable_material(kbl(results_df, caption = "p-hub AP40"),  c("striped", "hover", "condensed", "responsive"))
-
-file        <- "../data/p-hub/AP100.txt"
-p           <- 3
-times       <- 10
-results_df  <- test.hill.climbing(file, p, times)
-# Print results in an HTML Table
-kable_material(kbl(results_df, caption = "p-hub AP100"),  c("striped", "hover", "condensed", "responsive"))
-
-
-
-#Lo nuestro
-file        <- "../data/p-hub/AP100.txt"
-p           <- 3
-times       <- 10
-results_df  <- random.restart.hill.climbing(file, p, times)
-# Print results in an HTML Table
-kable_material(kbl(results_df, caption = "p-hub AP100"),  c("striped", "hover", "condensed", "responsive"))
