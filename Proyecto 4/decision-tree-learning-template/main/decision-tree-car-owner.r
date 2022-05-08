459f35b98ac892f8ebd382c0fd35063af336b0ae
asierbrizu <asierbrizu@opendeusto.es>
2022-05-08 15:07:36 +0200

    Cambios en archivos

--- Proyecto 4/decision-tree-learning-template/main/decision-tree-car-owner.r
+++ Proyecto 4/decision-tree-learning-template/main/decision-tree-car-owner.r
@@ -1,88 +1,62 @@
-# Clear plots
 if(!is.null(dev.list())) dev.off()
-# Clear console
 cat("\014") 
-# Clean workspace
 rm(list=ls())
-# Set working directory
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# Install required packages
-library(lattice)
 library(ggplot2)
+library(rpart.plot)
+library(lattice)
 library(caret)
 library(rpart)
-library(rpart.plot)
 
-# Read data from CSV
-filename = "../data/CAR DETAILS FROM CAR DEKHO.csv"
-data <- read.csv(file = filename, sep =",", header = TRUE)
+archivo = "../data/CAR DETAILS FROM CAR DEKHO.csv"
+data <- read.csv(file = archivo, sep =",", header = TRUE)
 
-# Transform and delete data
 data$name = NULL
 data <- transform(data, fuel = ifelse(fuel == "Petrol", 0, 
                                       ifelse(fuel == "Diesel", 1, 
                                              ifelse(fuel == "Electric", 2, 
                                                     ifelse(fuel == "LPG", 3, 4)))))
-
-# Percentaje of training examples
+precisionOptima <- 0
 training_p <- 0.8
+modeloOptimo    <- NULL
 
-best_model    <- NULL
-best_accuracy <- 0
 
 for (i in 1:10){
-  # Generate data partition 80% training / 20% test. The result is a vector with the indexes 
-  # of the examples that will be used for the training of the model.
   training_indexes <- createDataPartition(y = data$owner, p = training_p, list = FALSE)
+  training_data <- data[training_indexes, ]
+  test_data     <- data[-training_indexes, ]
+  modelo <- rpart(formula = owner ~year + km_driven + selling_price, data = training_data)
+  prediccion <- predict(modelo, test_data, type = "class")
+  resPrediccion <- table(test_data$owner, prediccion)
+  matrix <- confusionMatrix(resPrediccion)
+  precision <- matrix$overall[1]
   
-  # Split training and test data
-  training_data <- data[training_indexes, ]  # Extract training data using training_indexes
-  test_data     <- data[-training_indexes, ] # Extract data with the indexes not included in training_indexes 
-  
-  # Create Linear Model using training data. Formula = all the columns except Salary
-  #model <- rpart(formula = owner ~., data = training_data)
-  model <- rpart(formula = owner ~year + km_driven + selling_price, data = training_data)
-  
-  # Make the prediction using the model and test data
-  prediction <- predict(model, test_data, type = "class")
-  
-  # Calculate accuracy using Confusion Matrix
-  prediction_results <- table(test_data$owner, prediction)
-  matrix <- confusionMatrix(prediction_results)
-  accuracy <- matrix$overall[1]
-  
-  if (best_accuracy < accuracy){
-    best_accuracy <- accuracy
-    best_model    <- model
+  if (precisionOptima < precision){
+    modeloOptimo    <- modelo
+    precisionOptima <- precision
   }
 }
 
-# Print the accuracy
-accuracy <- paste0("Accuracy = ", round(100*best_accuracy, digits = 2), "%")
-print(accuracy, quote = FALSE)
-
-# Print attributes in descending relevance
-attrs <- names(best_model$variable.importance)
-
+precision <- paste0("Accuracy = ", round(100*precisionOptima, digits = 2), "%")
+print(precision, quote = FALSE)
+attrs <- names(modeloOptimo$variable.importance)
 print("Attributes in descending order of relevance")
 
 for (i in 1:length(attrs)) {
   print(paste0("  ", attrs[i]), quote = FALSE)
 }
 
-# Plot tree (this method is slow, wait until pot is completed)
-rpart.plot(model, 
+rpart.plot(modelo, 
            type = 2,
            extra = 102,
            tweak = 1.1,
            box.palette = "GnYlRd",
            shadow.col = "darkgray",
            main = "owner", 
-           sub = accuracy)
+           sub = precision)
 
-# Print the rules that represent the Decision Tree
-rpart.rules(model, 
+rpart.rules(modelo, 
             style="wide", 
             cover = TRUE, 
             eq = "==", 
--- Proyecto 4/decision-tree-learning-template/main/decision-tree-car-seller_type.r
+++ Proyecto 4/decision-tree-learning-template/main/decision-tree-car-seller_type.r
@@ -1,88 +1,62 @@
-# Clear plots
 if(!is.null(dev.list())) dev.off()
-# Clear console
 cat("\014") 
-# Clean workspace
 rm(list=ls())
-# Set working directory
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# Install required packages
-library(lattice)
 library(ggplot2)
+library(rpart.plot)
+library(lattice)
 library(caret)
 library(rpart)
-library(rpart.plot)
 
-# Read data from CSV
-filename = "../data/CAR DETAILS FROM CAR DEKHO.csv"
-data <- read.csv(file = filename, sep =",", header = TRUE)
+archivo = "../data/CAR DETAILS FROM CAR DEKHO.csv"
+data <- read.csv(file = archivo, sep =",", header = TRUE)
 
-# Transform and delete data
 data$name = NULL
 data <- transform(data, fuel = ifelse(fuel == "Petrol", 0, 
                                       ifelse(fuel == "Diesel", 1, 
                                              ifelse(fuel == "Electric", 2, 
                                                     ifelse(fuel == "LPG", 3, 4)))))
-
-# Percentaje of training examples
+precisionOptima <- 0
 training_p <- 0.8
+modeloOptimo    <- NULL
 
-best_model    <- NULL
-best_accuracy <- 0
 
 for (i in 1:10){
-  # Generate data partition 80% training / 20% test. The result is a vector with the indexes 
-  # of the examples that will be used for the training of the model.
   training_indexes <- createDataPartition(y = data$seller_type, p = training_p, list = FALSE)
+  training_data <- data[training_indexes, ]
+  test_data     <- data[-training_indexes, ]
+  modelo <- rpart(formula = seller_type ~km_driven + owner + selling_price, data = training_data)
+  prediccion <- predict(modelo, test_data, type = "class")
+  resPrediccion <- table(test_data$seller_type, prediccion)
+  matrix <- confusionMatrix(resPrediccion)
+  precision <- matrix$overall[1]
   
-  # Split training and test data
-  training_data <- data[training_indexes, ]  # Extract training data using training_indexes
-  test_data     <- data[-training_indexes, ] # Extract data with the indexes not included in training_indexes 
-  
-  # Create Linear Model using training data. Formula = all the columns except Salary
-  #model <- rpart(formula = seller_type ~., data = training_data)
-  model <- rpart(formula = seller_type ~km_driven + owner + selling_price, data = training_data)
-  
-  # Make the prediction using the model and test data
-  prediction <- predict(model, test_data, type = "class")
-  
-  # Calculate accuracy using Confusion Matrix
-  prediction_results <- table(test_data$seller_type, prediction)
-  matrix <- confusionMatrix(prediction_results)
-  accuracy <- matrix$overall[1]
-  
-  if (best_accuracy < accuracy){
-    best_accuracy <- accuracy
-    best_model    <- model
+  if (precisionOptima < precision){
+    modeloOptimo    <- modelo
+    precisionOptima <- precision
   }
 }
 
-# Print the accuracy
-accuracy <- paste0("Accuracy = ", round(100*best_accuracy, digits = 2), "%")
-print(accuracy, quote = FALSE)
-
-# Print attributes in descending relevance
-attrs <- names(best_model$variable.importance)
-
+precision <- paste0("Accuracy = ", round(100*precisionOptima, digits = 2), "%")
+print(precision, quote = FALSE)
+attrs <- names(modeloOptimo$variable.importance)
 print("Attributes in descending order of relevance")
 
 for (i in 1:length(attrs)) {
   print(paste0("  ", attrs[i]), quote = FALSE)
 }
 
-# Plot tree (this method is slow, wait until pot is completed)
-rpart.plot(best_model, 
+rpart.plot(modeloOptimo, 
            type = 2,
            extra = 102,
            tweak = 1.1,
            box.palette = "GnYlRd",
            shadow.col = "darkgray",
            main = "seller_type", 
-           sub = accuracy)
+           sub = precision)
 
-# Print the rules that represent the Decision Tree
-rpart.rules(best_model, 
+rpart.rules(modeloOptimo, 
             style="wide", 
             cover = TRUE, 
             eq = "==", 
