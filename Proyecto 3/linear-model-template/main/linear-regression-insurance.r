25c6f42fa7441273f408b87abb2afc576f26a030
Iker Morales <ikermorales@opendeusto.es>
2022-05-08 19:39:50 +0200

    Cambios importantes

--- Proyecto 3/linear-model-template/main/linear-regression-insurance.r
+++ Proyecto 3/linear-model-template/main/linear-regression-insurance.r
@@ -31,6 +31,23 @@ for (col_name in colnames(data)) {
   }
 }
 
+for(i in 1:10){
+  # Generate data partition 80% training / 20% test. The result is a vector with 
+  # the indexes of the examples that will be used for the training of the model.
+  training_indexes <- createDataPartition(y = data$charges, p = 0.8, list = FALSE)
+  
+  # Split training and test data
+  training_data <- data[training_indexes, ]  # Extract training data using training_indexes
+  test_data     <- data[-training_indexes, ] # Extract data with the indexes not included in training_indexes 
+  
+  # Create Linear Model using training data. Formula = all the columns except Salary
+  model <- lm(formula = charges ~., data = training_data)
+  print(model)
+  
+  # Make the prediction using the model and test data
+  prediction <- predict(model, test_data)
+}
+
 # Correlacion
 print("Correlación entre cada atributo y coste: Baja correlacion (-0.2 < x < 0.2)", quote=FALSE)
 
@@ -73,6 +90,41 @@ print(paste0("- Error medio medio del mejor modelo: ", result$best_error))
 # Resumen mejor modelo
 summary(result$best_model)
 
+solucion <- data.frame(i1 = 0, c1 = 0,
+                       i2 = 0, c2 = 0,
+                       i3 = 0, c3 = 0)
+
+
+for(i in 1:length(data$smoker)){
+  aumento <- transform(data, age = age+5)
+  aumentos <- predict(model, aumento)
+  aumento <- transform(aumento, charges = aumentos - charges)
+  resta <- (data[i,]$charges - aumento[i,]$charges)*(-1)
+  
+  if(solucion$c1 < resta){
+    solucion$c3 <- solucion$c2
+    solucion$i3 <- solucion$i2
+    
+    solucion$c2 <- solucion$c1
+    solucion$i2 <- solucion$i1
+    
+    solucion$c1 <- resta
+    solucion$i1 <- i
+    
+  }else if(solucion$c2 < resta){
+    solucion$c3 <- solucion$c2
+    solucion$i3 <- solucion$i2
+    
+    solucion$c2 <- resta
+    solucion$i2 <- i
+    
+  }else if(solucion$c2 < resta){
+    solucion$c3 <- resta
+    solucion$i3 <- i
+    
+  }
+}
+
 #Esto es lo referente a la primera cuestion
 data_ns        <- data[20,]
 data_ns$smoker <- 0
@@ -90,10 +142,19 @@ print(data[3,])
 print(data_ys)
 
 #Esto es lo referente a la tercera cuestion
-data_5 <- transform(data, age = age + 5)
-prediction <- predict(result$best_model, data_5)
-data_5     <- transform(data_5, charges = prediction - charges)
-data_5_or  <- data_5[order(data_5$charges, decreasing=TRUE),]
-print("Las 3 personas con mayor coste son:")
-print(data_5_or[1:3,])
+
+#Primera persona
+print(data[solucion$i1,])
+print(solucion$c1)
+
+#Segunda persona
+print(data[solucion$i2,])
+print(solucion$c2)
+
+#Tercera persona
+print(data[solucion$i3,])
+print(solucion$c3)
+
+
+
 
